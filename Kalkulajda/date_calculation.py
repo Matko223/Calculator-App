c23c03c5028f235ed441123d31e5551aec9a79b8
Martin Valapka <martin.valapka1@gmail.com>
2024-09-09 23:20:33 +0200

    Date: Basic design of the combo boxes

--- Kalkulajda/date_calculation.py
+++ Kalkulajda/date_calculation.py
@@ -46,21 +46,104 @@ class DateCalculation(QWidget):
 
         # Adjust the startDate label
         self.startDate = QLabel("Start date", self.displayFrame)
-        self.startDate.setStyleSheet(f"color: white; font-size: 20px; font-weight: bold; margin-top: 3px;")
+        self.startDate.setStyleSheet(f"color: white; font-size: 25px; font-weight: bold; margin-top: 3px;")
         self.startDate.setAlignment(Qt.AlignCenter)
         frame_layout.addWidget(self.startDate, 0, 1, Qt.AlignCenter)
 
+        frame_layout.addItem(QSpacerItem(0, 20, QSizePolicy.Minimum, QSizePolicy.Fixed), 1, 1)
+
+        # Add comboboxes for day and month, and text field for year
+        date_input_layout = QHBoxLayout()
+        date_input_layout.setSpacing(10)
+
+        combobox_style = """
+        QComboBox {
+            color: white;
+            background-color: #4F4F4F;
+            font-size: 16px;
+            border-top-left-radius: 10px;
+            border-top-right-radius: 10px;
+            border-bottom-left-radius: 10px;
+            border-bottom-right-radius: 10px;
+            font-weight: bold;
+            padding: 5px;
+            padding-right: 20px;
+        }
+        QComboBox:!editable, QComboBox::drop-down:editable {
+            background-color: #4F4F4F;
+        }
+        QComboBox:!editable:on, QComboBox::drop-down:editable:on {
+            background-color: #4F4F4F;
+        }
+        QComboBox:on {
+            border-bottom-left-radius: 0px;
+            border-bottom-right-radius: 0px;
+        }
+        QComboBox QAbstractItemView::item:hover {
+            border: none;
+        }
+        QComboBox::drop-down {
+            subcontrol-origin: padding;
+            subcontrol-position: top right;
+            width: 20px;
+            border-left-width: 0px;
+            border-top-right-radius: 10px;
+        }
+        QComboBox::down-arrow {
+            image: url(./Pictures/60995.png);
+            width: 12px;
+            height: 12px;
+        }
+        QComboBox QAbstractItemView {
+            color: white;
+            background-color: #4F4F4F;
+            selection-background-color: #666666;
+            border-bottom-left-radius: 10px;
+            border-bottom-right-radius: 10px;
+        }
+        QComboBox::item {
+            padding: 4px;
+        }
+        QComboBox::item:selected {
+            background-color: #666666;
+        }
+        """
+
+        day_combobox = QComboBox(self.displayFrame)
+        day_combobox.addItems([str(i) for i in range(1, 32)])
+        day_combobox.setStyleSheet(combobox_style)
+        day_combobox.setFixedSize(120, 40)
+
+        month_combobox = QComboBox(self.displayFrame)
+        month_combobox.addItems(['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
+                                 'September', 'October', 'November', 'December'])
+        month_combobox.setStyleSheet(combobox_style)
+        month_combobox.setFixedSize(120, 40)
+
+        year_input = QLineEdit(self.displayFrame)
+        year_input.setStyleSheet("color: white; background-color: #4F4F4F; font-size: 16px; border-radius: 10px; "
+                                 "font-weight: bold; padding: 5px;")
+        year_input.setFixedSize(120, 40)
+        year_input.setPlaceholderText("Year")
+
+        date_input_layout.addWidget(day_combobox)
+        date_input_layout.addWidget(month_combobox)
+        date_input_layout.addWidget(year_input)
+
+        # Add the combobox layout after the spacer
+        frame_layout.addLayout(date_input_layout, 2, 1, Qt.AlignCenter)
+
         # Adjust the endDate label
         self.endDate = QLabel("End date", self.displayFrame)
-        self.endDate.setStyleSheet(f"color: white; font-size: 20px; font-weight: bold; margin-top: 100px;")
+        self.endDate.setStyleSheet(f"color: white; font-size: 25px; font-weight: bold; margin-top: 50px;")
         self.endDate.setAlignment(Qt.AlignCenter)
-        frame_layout.addWidget(self.endDate, 1, 1, Qt.AlignCenter)
+        frame_layout.addWidget(self.endDate, 3, 1, Qt.AlignCenter)
 
         # Add the resultLabel
         self.resultLabel = QLabel("Result", self.displayFrame)
-        self.resultLabel.setStyleSheet(f"color: white; font-size: 20px; font-weight: bold; margin-top: 150px;")
+        self.resultLabel.setStyleSheet(f"color: white; font-size: 25px; font-weight: bold; margin-top: 100px;")
         self.resultLabel.setAlignment(Qt.AlignCenter)
-        frame_layout.addWidget(self.resultLabel, 3, 1, Qt.AlignCenter)
+        frame_layout.addWidget(self.resultLabel, 4, 1, Qt.AlignCenter)
 
         # Add the calculateButton, slightly above the resultLabel
         self.calculateButton = QPushButton("Calculate", self.displayFrame)
@@ -68,10 +151,10 @@ class DateCalculation(QWidget):
             f"background-color: {ORANGE}; color: white; font-size: 20px; font-weight: bold; border-radius: 10px;")
         self.calculateButton.setFixedSize(180, 50)
         self.calculateButton.clicked.connect(self.calculate)
-        frame_layout.addWidget(self.calculateButton, 3, 1, Qt.AlignCenter)
+        frame_layout.addWidget(self.calculateButton, 5, 1, Qt.AlignCenter)
 
         # Add a vertical spacer to ensure there is no extra space on the sides
-        frame_layout.addItem(QSpacerItem(0, 40, QSizePolicy.Minimum, QSizePolicy.Expanding), 3, 1)
+        frame_layout.addItem(QSpacerItem(0, 40, QSizePolicy.Minimum, QSizePolicy.Expanding), 6, 1)
 
     def calculate(self):
         pass
