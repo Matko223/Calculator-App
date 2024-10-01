64321c46a20cb0c42b17d5a24288f37a37f7cc89
Martin Valapka <martin.valapka1@gmail.com>
2024-10-01 22:42:05 +0200

    Added the the API for currency conversion

---
+++ Kalkulajda/currency_api.py
@@ -0,0 +1,24 @@
+"""
+@file currency_converter.py
+@brief File containing Api for currency conversion.
+
+@author Martin Valapka (xvalapm00)
+@date 01.10. 2024
+"""
+
+import requests
+
+API_URL = "https://api.exchangerate-api.com/v4/latest"
+
+
+def get_exchange_rate(base_currency, target_currency):
+    url = f"{API_URL}{base_currency.upper()}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        rates = data.get('rates', {})
+        return rates.get(target_currency.upper(), None)
+    else:
+        print(f"Error fetching data: {response.status_code}")
+        return None
