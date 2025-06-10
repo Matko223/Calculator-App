b70b05994c2fb60595622185819ecb902e375504
Martin Valapka <val.24.mar@gmail.com>
2025-06-10 18:00:07 +0200

    Bug fix

--- src/img_path.py
+++ src/img_path.py
@@ -6,6 +6,7 @@
 """
 
 import os
+import sys
 
 def resource_path(relative_path):
     """
@@ -16,7 +17,7 @@ def resource_path(relative_path):
     try:
         base_path = sys._MEIPASS
     except Exception:
-        base_path = os.path.abspath(".")
+        base_path = os.path.dirname(os.path.abspath(__file__))
+        base_path = os.path.dirname(base_path)
     
     return os.path.join(base_path, relative_path)
-    
