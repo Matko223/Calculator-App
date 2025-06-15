34f8ee1744a954c48498e9e948a2594c6fdaac8c
Martin Valapka <val.24.mar@gmail.com>
2025-06-15 17:10:34 +0200

    Separated main

---
+++ src/main.py
@@ -0,0 +1,28 @@
+"""
+@file: main.py
+@brief: Main entry point for the Calculator application.
+
+@author: Martin Valapka
+"""
+
+import sys
+import os
+import ctypes
+from PySide6.QtWidgets import QApplication
+from PySide6.QtGui import QIcon
+
+from calculators.standard.calculator import App
+from utils.resource_manager import resource_path
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+if os.name == "nt":
+    icon_path = resource_path(os.path.join('icons', 'real_logo.png'))
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(icon_path)
+
+    window = App()
+    window.show()
+    sys.exit(app.exec())
+
