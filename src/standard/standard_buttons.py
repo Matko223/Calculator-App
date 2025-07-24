93496b8a70996fa0828aa220d437523bb8ba2b37
Martin Valapka <val.24.mar@gmail.com>
2025-07-24 22:32:06 +0200

    Added new files

---
+++ src/standard/standard_buttons.py
@@ -0,0 +1,33 @@
+"""
+@file standard_buttons.py
+@brief This module provides the standard buttons for the calculator application.
+
+@author: Martin Valapka
+"""
+
+import sys
+import os
+from decimal import getcontext, Decimal
+from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QGridLayout, QLabel, QPushButton, QHBoxLayout
+from PySide6.QtGui import QFont, QKeySequence, QShortcut, QIcon
+from PySide6.QtCore import Qt, QSize
+from standard import mathlib
+from currency.currency_converter import CurrencyConverter
+from day.date_calculation import DateCalculation
+from help.help_menu import HelpWindow
+from sidebar.mode_menu import Sidebar
+from bmi.bmi_calculator import BMICalculator
+from expression.photomath_mode import PhotomathMode
+from settings.settings import Settings
+from utils.img_path import resource_path
+import ctypes
+
+# Color definitions
+LIGHT_GRAY = "#979797"
+DARK_GRAY = "#3D3D3D"
+ORANGE = "#FFA500"
+GRAY = "#808080"
+COLOR_REST = "#4F4F4F"
+LABEL_COLOR = "#25265E"
+HOVER_COLOR = "#898989"
+HOVER_OPERATOR = "#FF8409"
